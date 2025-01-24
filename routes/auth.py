c5347da80da5740a6c2426d7dc8aac8d7838a498
19921876829@163.com <19921876829@163.com>
2025-01-23 21:49:05 -0600

    feat: RBAC function finished

--- routes/auth.py
+++ routes/auth.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, jsonify, request
 from werkzeug.security import check_password_hash
-from flask_jwt_extended import create_access_token, jwt_required, unset_jwt_cookies
+from flask_jwt_extended import create_access_token, jwt_required, unset_jwt_cookies, get_jwt_identity
 from models.user import User
+from utils.decorators import role_required
 
 auth_bp = Blueprint('auth', __name__)
 
@@ -19,9 +20,10 @@ def login():
     user = User.query.filter_by(email=email, is_deleted=False).first()
     if not user or not check_password_hash(user.password, password):
         return jsonify({"error": "Invalid email or password"}), 401
-
+    identity = {"id": user.id, "email": user.email, "role": user.role}
+    print("Identity:", identity)
     # 创建 JWT Token
-    access_token = create_access_token(identity={"id": user.id, "email": user.email})
+    access_token = create_access_token(identity=identity)
     return jsonify({"message": "Login successful", "access_token": access_token}), 200
 
 # 用户登出
@@ -32,3 +34,12 @@ def logout():
     # 清除 JWT Token
     unset_jwt_cookies(response)
     return response, 200
+
+# admin only route
+@auth_bp.route('/admin-only', methods=['GET'])
+@jwt_required()
+@role_required('admin')  # 使用装饰器限制角色
+def admin_only_route():
+    identity = get_jwt_identity()
+    print("JWT Identity:", identity)  # 打印当前身份信息
+    return jsonify({"message": "Welcome, Admin!"}), 200
