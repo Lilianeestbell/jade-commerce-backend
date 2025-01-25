9af4cb262b716a5c0855126d9fdb4aa65b0c2d67
19921876829@163.com <19921876829@163.com>
2025-01-25 10:49:55 -0600

    feat: add docker

--- config.py
+++ config.py
@@ -1,6 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+# 加载 .env 文件
+load_dotenv()
+
 class Config:
-    SECRET_KEY = '98b6f635464ba8a85f0585d136a7b4b75635d6ed49237352c1fa4d5c1b2a0f94'  # 应用的通用密钥
-    JWT_SECRET_KEY = '75ca5abe26d94f859f1ea80e1b043918d6843ef779ebe19d75637812f9b70d96e'  # JWT 的密钥
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:free1997@localhost/jade_commerce'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_ACCESS_TOKEN_EXPIRES=3600
+    SECRET_KEY = os.getenv("SECRET_KEY", "default_secret_key")
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "default_jwt_secret_key")
+    SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI", "sqlite:///test.db")
+    SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv("SQLALCHEMY_TRACK_MODIFICATIONS", False)
+    JWT_ACCESS_TOKEN_EXPIRES = int(os.getenv("JWT_ACCESS_TOKEN_EXPIRES", 3600))
